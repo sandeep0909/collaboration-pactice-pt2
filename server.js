7ff7057f4bf5d23b3d5d1a833de5e99a24af334e
Anil Rayamajhi <anilrm07@gmail.com>
2016-09-30 15:15:25 -0700

    Server Work Done

---
+++ server.js
@@ -0,0 +1,34 @@
+var
+  express = require('express'),
+  app = express(),
+  mongoose = require('mongoose'),
+  bodyParser = require('body-parser'),
+  logger = require('morgan'),
+  // bikeRoutes = require('./routes/bikes.js');
+
+
+////////Mongoose Connections//////////////
+mongoose.connect('mongodb://localhost/bike-shop', function(err) {
+  if(err){console.log("Problem connecting to Mongo");}
+  else {console.log("Connected to Mongo. Boom!");}
+});
+/////////////////////////////////////
+
+
+///////Middleware//////
+app.user(log('dev'));
+app.use(bodyParser.json());
+//////////////////////
+
+
+app.get('/', function(req, res) {
+  res.json({message: "COLLABORATION PROJECT INITIALISED"});
+});
+
+
+app.use('/', bikeRoutes);
+
+
+app.listen(3000, function() {
+  console.log("Server is running on 3000");
+});
